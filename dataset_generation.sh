2a76131ee09cc5247fd66d927dbf0b9b6f98b526
Matthew Feickert <matthew.feickert@cern.ch>
2025-10-28 11:49:18 -0600

    debug: Test with one job on simple_fig1_DAG.txt

--- dataset_generation.sh
+++ dataset_generation.sh
@@ -5,6 +5,8 @@
 JOB_ID="$1"
 
 nvidia-smi
-python dataset_generation.py @generation_arguments/galaxy_rot_curve_DAG.txt --sample_seed ${JOB_ID} --job_id ${JOB_ID}
+
+# python dataset_generation.py @generation_arguments/galaxy_rot_curve_DAG.txt --sample_seed ${JOB_ID} --job_id ${JOB_ID}
+python dataset_generation.py @generation_arguments/simple_fig1_DAG.txt --sample_seed ${JOB_ID} --job_id ${JOB_ID}
 
 echo "Finished job ${JOB_ID} on $(whoami)@$(hostname)"
